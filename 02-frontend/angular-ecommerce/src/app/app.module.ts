100b015131bc9586db312a43a80d0288537cd04e
youyyyyy <you519900911@gmail.com>
2023-02-07 23:30:00 -0500

    search by category static for ecommerce 2.0

--- 02-frontend/angular-ecommerce/src/app/app.module.ts
+++ 02-frontend/angular-ecommerce/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +21,7 @@ import { ProductService } from './services/product.service';
     ProductListComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(routes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- 02-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 02-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,25 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
-  // templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute) { } // the current active route that loaded the component. Useful for accessing route parameters
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe( // asyn assign results to the Product array
+    // check if "id" parameter is available
+    // route - use the activated route
+    // snapshot - state of route at this given moment in time
+    // paramMap - map of all route parameters
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string, convert string to a number using the "+" symbol
+      // "!" is the non-null assertion operator, tells compiler that the object is not null
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // no category id available... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe( // asyn assign results to the Product array
       data => {
         this.products = data;
       }

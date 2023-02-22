13bbe79b5ef4c9572ba32f5e777d06fa80f43a2e
youyyyyy <you519900911@gmail.com>
2023-02-22 00:06:48 -0500

    security login logout for ecommerce 3.0

---
+++ 02-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+      clientId: '0oa8f71wigy57L0Qv5d7',
+      issuer: 'https://dev-23985086.okta.com/oauth2/default',
+      redirectUri: 'http://localhost:4200/login/callback',
+      scopes: ['openid', 'profile', 'email']
+  }
+
+}

57ede1d81040b408371226df6a7f6e8e6ae53aea
youyyyyy <you519900911@gmail.com>
2023-02-22 21:07:35 -0500

    security handling browser refresh for ecommerce 3.0

--- 02-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 02-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -14,7 +14,22 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  // persist cart item
+  // storage: Storage = sessionStorage; // session storage, data will be lost if we close the browser
+  storage: Storage = localStorage; // data is persisted and survives browser starts
+
+  constructor() {
+
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+    if (data != null) {
+      this.cartItems = data;
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+  }
 
   addToCart(theCartItem: CartItem) {
 
@@ -58,6 +73,13 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist cart data
+    this.persistCartItem();
+  }
+
+  persistCartItem() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
